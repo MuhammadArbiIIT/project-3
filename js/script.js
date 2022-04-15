62f0b31af539ba334ad5fa55977b3e0c14fba424
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-04-15 03:06:22 -0500

    add picture and add script file into one

--- js/script.js
+++ js/script.js
@@ -41,7 +41,27 @@ function initMap() {
         }
     }
 
-
+    //slider
+    var slider_img = document.querySelector('.slider-img');
+    var images = ['tech.jpg', 'pakistanflag.jpg','game2.jpg','iit_banner.png'];var i = 0;
+    
+    function prev(){
+      if(i <= 0) i = images.length;	
+      i--;
+      return setImg();			 
+    }
+    
+    function next(){
+      if(i >= images.length-1) i = -1;
+      i++;
+      return setImg();			 
+    }
+    
+    function setImg(){
+      return slider_img.setAttribute('src', "resources/" + images[i]);
+      
+    }
+    
 
 
 
