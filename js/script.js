45f05b3261a66e9c99792cf30d2b5391fe330cab
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-04-15 00:36:52 -0500

    marker 1 home

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 41.8781, lng: -87.6298},
-        zoom: 8,
-        mapId: '684f0b988910820c'
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.8781, lng: -87.6298},
+    zoom: 10,
+    mapId: '684f0b988910820c'
+    });
+      
+    const marker = new google.maps.Marker({
+      position: {lat:  42.01376708919888, lng: -87.68298102057864},
+      map,
+      title: "home",
       });
+    
+      const infowindow = new google.maps.InfoWindow({
+        content: "This is where I live",
+      });
+
+      marker.addListener("click", () => {
+        infowindow.open(map, marker);
+        });
 }
